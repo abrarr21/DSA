ceacb53e3f456eb8ad369c77c4c70d4d5bce0fcf
abrarr21 <abrarrr2110@gmail.com>
2025-02-05 20:19:57 +0530

    patternPractice

--- patterns/one.cpp
+++ patterns/one.cpp
@@ -132,12 +132,123 @@ void printEleven(int n) {
     space -= 2;
   }
 }
+
+void printTwelve(int n) {
+  int num = 1;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << num << " ";
+      num += 1;
+    }
+    cout << endl;
+  }
+}
+
+void printThirteen(int n) {
+  for (int i = 0; i < n; i++) {
+    for (char ch = 'A'; ch <= 'A' + i; ch++) {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+
+void printFourteen(int n) {
+  for (int i = 0; i < n; i++) {
+    for (char ab = 'A'; ab <= 'A' + (n - i - 1); ab++) {
+      cout << ab << " ";
+    }
+    cout << endl;
+  }
+}
+
+void printFifteen(int n) {
+  for (int i = 0; i < n; i++) {
+    char ch = 'A' + i;
+    for (int j = 0; j <= i; j++) {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+
+void printSixteen(int n) {
+  for (int i = 0; i < n; i++) {
+    // Space
+    for (int j = 0; j <= (n - i - 1); j++) {
+      cout << " ";
+    }
+    // Character
+    char ch = 'A';
+    int breakpoint = (2 * i + 1) / 2;
+    for (int j = 0; j < 2 * i + 1; j++) {
+      cout << ch;
+      if (j < breakpoint)
+        ch++;
+      else
+        ch--;
+    }
+    // Space
+    for (int j = 0; j <= (n - i - 1); j++) {
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
+void printSeventeen(int n) {
+  for (int i = 0; i < n; i++) {
+    for (char ch = 'E' - i; ch <= 'E'; ch++) {
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+}
+
+void printEighteen(int n) {
+  int initialNumber = 0;
+  for (int i = 0; i < n; i++) {
+    // Stars
+    for (int j = 0; j < n - i; j++) {
+      cout << "* ";
+    }
+    // Space
+    for (int j = 0; j < initialNumber; j++) {
+      cout << "  ";
+    }
+    // Stars
+    for (int j = 0; j < n - i; j++) {
+      cout << "* ";
+    }
+    cout << endl;
+    initialNumber = initialNumber + 2;
+  }
+
+  initialNumber = 2 * n - 2;
+  for (int i = 0; i < n; i++) {
+    // Stars
+    for (int j = 1; j <= i; j++) {
+      cout << "* ";
+    }
+    // Space
+    for (int j = 0; j < initialNumber; j++) {
+      cout << "  ";
+    }
+    // Stars
+    for (int j = 1; j <= i; j++) {
+      cout << "* ";
+    }
+    initialNumber = initialNumber - 2;
+    cout << endl;
+  }
+}
+
 int main() {
   int testcases;
   cin >> testcases;
   for (int i = 0; i < testcases; i++) {
     int n;
     cin >> n;
-    printEleven(n);
+    printEighteen(n);
   }
 }
